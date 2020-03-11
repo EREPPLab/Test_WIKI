51ac9787dba3a63fe419f401d98cb4c092a2a085
EREPPLab <60016577+EREPPLab@users.noreply.github.com>
2020-03-11 13:13:03 -0400

    Delete GenerationTermination.py

--- Termination/GenerationTermination.py
+++
@@ -1,9 +0,0 @@
-from Termination.Termination import Termination
-
-
-class GenerationTermination(Termination):
-    def __init__(self, max_generation):
-        self.max_generation = max_generation
-
-    def is_over(self, ga):
-        return ga.generation >= self.max_generation
